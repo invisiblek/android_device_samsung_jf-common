e27f4512656929db70613d68dc640d14c3389361
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-24 12:59:27 -0500

    jf-common: fix extraction scripts for vendor/lineage

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -32,7 +32,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and common guards
 write_headers "jactivelte jflteatt jfltespr jfltetmo jfltevzw jfltexx jfltecan jflteusc jfltecri jfltecsp jfltezm jftddxx"
@@ -42,7 +42,7 @@ write_makefiles "$MY_DIR"/common-proprietary-files.txt
 write_footers
 
 # Reinitialize the helper for device
-setup_vendor "$BLOB_LOC" "$VENDOR" "$CM_ROOT"
+setup_vendor "$BLOB_LOC" "$VENDOR" "$LINEAGE_ROOT"
 
 # Copyright headers and guards
 write_headers
